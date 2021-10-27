bb319fecf44fbea5c1f6408d94b1f64c672cbba1
Mandiscribe <93286166+Mandiscribe@users.noreply.github.com>
2021-10-28 03:12:58 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 sudo apt-get install clang
 clang++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -pthread -std=c++11 -fpermissive -o createid -w #Build source code into executable
 sudo apt-get install boxes
-echo -e "cidnya udah bisa di create nih sekarang tinggal /createid untuk membuat cid!"| boxes -d parchment -a c
+echo -e "cidnya udah bisa di create nih sekarang tinggal ./createid untuk membuat cid!"| boxes -d parchment -a c
