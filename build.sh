fddc12403a1849df15f7b258ca0f4a8c7a71f0b8
Mandiscribe <93286166+Mandiscribe@users.noreply.github.com>
2021-10-28 02:49:48 +0700

    Delete build.sh

--- build.sh
+++
@@ -1,3 +0,0 @@
-# If you already install g++ or build-essential, use this
-clang++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -Wall -std=c++11 -fpermissive -o start -w #Build source code into executable
-echo "Cid nya udah bisa, sekarang ketik ./start untuk memulai cid bot."
